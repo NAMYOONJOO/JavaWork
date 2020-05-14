ebc6f89ff497fdfb9d926fcca97ee1fe15ac0778
NAMYOONJOO <bhd4445@naver.com>
2020-05-14 17:45:16 +0900

    JavaWork commit 2020-05-14

--- Lec_WEB/WebContent/myweb/GoldCoffeeJS.js
+++ Lec_WEB/WebContent/myweb/GoldCoffeeJS.js
@@ -1,13 +1,37 @@
 $(document).ready(function(){
 
-$('div#top').on({mouseenter:function(){
-    $('div#top').css("background-color","rgba(20,20,20,1)")
-},
-mouseleave:function(){
-    $('div#top').css("background-color","rgba( 255, 255, 255, 0.2 )")
+    $('div#top').on({mouseenter:function(){
+        $('div#top').css("background-color","rgba(20,20,20,1)")
+    },
+    mouseleave:function(){
+        $('div#top').css("background-color","rgba( 255, 255, 255, 0.2 )")
 
-},
+    },
+    })
+
+    
+    function main_content(){
+        
+        $('div.main_content').fadeOut(0)
+        $('div#content1').fadeIn(0)
+        $('div#content1').delay(8000).fadeOut(1000)
+        $('div#content2').delay(8000).fadeIn(1000).promise()
+        .then(function(){
+            $('div#content2').delay(8000).fadeOut(1000)
+            return $('div#content3').delay(8000).fadeIn(1000).promise()
+        })
+        .then(function(){
+            $('div#content3').delay(8000).fadeOut(1000)
+            return $('div#content1').delay(8000).fadeIn(1000).promise()
+        })
+        .then(main_content)
+
+    }
+
+    main_content()
+   
+
+
 })
 
-})
 
