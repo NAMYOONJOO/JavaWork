4fe7832cc776c1bb551b10039d1a4689b7b1f8f6
NAMYOONJOO <bhd4445@naver.com>
2020-05-15 17:46:00 +0900

    JavaWork commit 2020-05-15

---
+++ Lec_WEB/WebContent/JS_core/01_basic/17_객체_getter_setter.js
@@ -0,0 +1,102 @@
+/* object 의 getter, setter
+
+getter 함수: 특정 값을 조회할때마다
+setter 함수: 특정 값을 설정할때마다
+
+https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Functions/get
+
+ES 5.1 에 최초 정의
+https://www.ecma-international.org/ecma-262/5.1/#sec-11.1.5
+
+ES 6 에서 계산된 프로퍼티 이름이 추가
+https://www.ecma-international.org/ecma-262/6.0/#sec-method-definitions
+
+
+JS 에서 매우 유용
+
+*/
+
+const numbers = {
+    a: 1, b:2,
+
+    //getter 함수
+    get sum(){
+        //get 키워드로 시작
+        console.log("sum 함수가 실행됩니다.")
+        //반.드.시 무엇인가 리턴해주어야 한다. 
+        return this.a+this.b;
+        //return 이 빠지면 undefined 리턴
+
+        
+    }
+    }
+
+    //getter 함수는 아래와 같이 '조회' 하려 할 때
+    //함수가 호출, 실행 됩ㄴ디ㅏ. 
+    // console.log(numbers.sum());//에러
+    console.log(numbers.sum)
+    
+    numbers.b = 5
+    
+    console.log(numbers.sum)
+    //setter함수
+    const dog = {
+        _name :'멍멍이',
+
+        //setter함수 정의
+        //set 키워드로 시작
+        set name(value){
+
+            //반드시 parameter설정되어야 함 !
+            console.log('이름이 바뀝니다..'+value);
+            this._name = value        }
+    }
+    console.log();
+    console.log(dog._name)
+    // dog.name('뽀삐') //이건 에러 직접적 호출 안댐
+    dog.name = '뽀ㅃㅣ' //setter name 호출
+    console.log(dog._name);
+    console.log();
+
+    console.log('-------------------------')
+
+    const numbers2 = {
+        _a : 1,
+        _b : 2,
+        sum : function(){
+            return this._a+this._b;
+        }
+    }
+
+    console.log(numbers2.sum())
+    
+    numbers2._a  =10; numbers2._b = 20;
+    console.log(numbers2.sum())
+    console.log(numbers2.sum())
+    console.log(numbers2.sum())
+    console.log(numbers2.sum())
+    console.log(numbers2.sum())
+    console.log(numbers2.sum())
+
+    const numbers3 = {
+        _a : 1,
+        _b : 2,
+        sum : 3,
+
+        get a() {return this._a},
+        get b() {return this._b},
+        set a(value){
+            this._a = value;
+            this.sum = this._a+this._b;
+        },
+        set b(value){
+            this._b = value;
+            this.sum = this._a+this._b;
+        }
+    }
+
+    console.log('------------------------------')
+    console.log(numbers3.sum);
+    numbers3.a = 10;//setter동작
+    numbers3.b = 20;//setter동작
+    console.log(numbers3.sum);
--- Lec_WEB/WebContent/myweb/GoldCoffeeJS.js
+++ Lec_WEB/WebContent/myweb/GoldCoffeeJS.js
@@ -29,9 +29,108 @@ $(document).ready(function(){
     }
 
     main_content()
+
+    //Page2
+    
+    //order 팝업
+    var modal = document.getElementById('orderpopup1')
+    var btn = document.getElementById('orderbtn1')
+    var modal2 = document.getElementById('orderpopup2')
+    var orderbtn = document.getElementById('orderbtn')
+    var span = document.getElementsByClassName('close')[0]
+
+    btn.onclick = function(){
+        modal.style.display = "block"
+    }
+    orderbtn.onclick = function(){
+        modal2.style.display = "block"
+        modal.style.display = "none"
+    }
+
+    span.onclick = function(){
+        modal.style.display='none'
+      }
+      // 팝업창 바깥을 클릭해도 팝업이 닫히게 하기 (modal)<--->(반대 : modeless)
+      window.onclick = function(event){
+        if(event.target==modal){
+          modal.style.display = 'none'
+        }
+      
+      }
+      //order popup menubar---------------------------------------------------
+      
+      var menu1 = document.getElementById('Omenu1') 
+      var menu2 = document.getElementById('Omenu2') 
+      var menu3 = document.getElementById('Omenu3')
+      var menubtn1 = document.getElementById('menu1') 
+      var menubtn2 = document.getElementById('menu2') 
+      var menubtn3 = document.getElementById('menu3')
+      menubtn1.onclick = function(){
+            menubtn1.style.backgroundColor='rgb(236,150,120)'
+            menubtn1.style.color='#fff'
+            menubtn2.style.backgroundColor='#eee'
+            menubtn2.style.color='#444'
+            menubtn3.style.backgroundColor='#eee'
+            menubtn3.style.color='#444'
+
+            menu1.style.display = 'block'
+            menu2.style.display = 'none'
+            menu3.style.display = 'none'
+        }
+    menubtn2.onclick = function(){
+            menubtn2.style.backgroundColor='rgb(236,150,120)'
+            menubtn2.style.color='#fff'
+            menubtn1.style.backgroundColor='#eee'
+            menubtn1.style.color='#444'
+            menubtn3.style.backgroundColor='#eee'
+            menubtn3.style.color='#444'
+
+            menu1.style.display = 'none'
+            menu2.style.display = 'block'
+            menu3.style.display = 'none'
+    }
+    menubtn3.onclick = function(){
+            menubtn3.style.backgroundColor='rgb(236,150,120)'
+            menubtn3.style.color='#fff'
+            menubtn2.style.backgroundColor='#eee'
+            menubtn2.style.color='#444'
+            menubtn1.style.backgroundColor='#eee'
+            menubtn1.style.color='#444'
+            menu1.style.display = 'none'
+            menu2.style.display = 'none'
+            menu3.style.display = 'block'
+    }
+    //order list
+    var orderlist= "";
+    var price = 0;
+    var menucount =0;
+    var ordermenu = document.getElementsByClassName('ordermenu')
+    // alert($('#menu3-1').text())
+    for(i = 0;i<ordermenu.length;i++){
+        ordermenu[i].addEventListener('click',addmenulist)
+
+    }
+    function addmenulist(){
+        orderlist+=$(this).text()+"<br>"
+        menucount++;
+        price += $(this).val()
+        $('#orderlist').html(orderlist)
+        $('#orderprice').text("총 "+menucount+"잔 "+price+" 원 ")
+    }
+    // $('#orderlist').text($('#menu3-1').text())
    
 
 
+
+    
+   
 })
+var didScroll;
 
+$(window).scroll(function(event){ didScroll = true; });
+setInterval(function() { if (didScroll) { hasScrolled(); didScroll = false; } }, 250);
 
+function hasScrolled() {
+    $('div#top').css("background-color","rgba(20,20,20,1)")
+   
+}
